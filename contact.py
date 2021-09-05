0aaee73e41e057a76b796d2344e8a8832273b780
halima254 <halima.yahya@student.moringaschool.com>
2021-09-06 01:49:05 +0300

    add init method to create new instances of class contact

--- contact.py
+++ contact.py
@@ -7,6 +7,18 @@ class Contact:
     
 
     contact_list = []
+    def __init__(self, user_name, password):
+        
+        '''
+        __init__ method helps in defining properties for our objects.
+
+        Args:
+            user_name : New contact user name.
+            password: New contact password.
+           
+        '''
+        self.user_name = user_name
+        self.password= password
 
     
     
