cb779633b85d250c847062f4ad1e26c964532253
halima254 <halima.yahya@student.moringaschool.com>
2021-09-06 01:45:00 +0300

    add initial files

---
+++ contact.py
@@ -0,0 +1,21 @@
+import pyperclip
+import random
+class Contact:
+    '''
+    class contacts generates new instances of a class
+    '''
+    
+
+    contact_list = []
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+   
+      
