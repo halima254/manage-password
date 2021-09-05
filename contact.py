be98ab2fe6e308bea98d959e8e50a55143755db4
halima254 <halima.yahya@student.moringaschool.com>
2021-09-06 01:56:01 +0300

    add method decorator that defines, stores and deletes credentials

--- contact.py
+++ contact.py
@@ -37,7 +37,46 @@ class Credentials():
     Create credentials class to help create new objects of credentials
     """
     credentials_list = []       
+    @classmethod
+    def verify_contact(cls,user_name, password):
+        """
+        method to verify whether the user is in the contact_list or not
+        """
+        a_cont = ""
+        for user in Contact.contact_list:
+            if(contact.user_name == user_name and contact.password == password):
+                    a_cont == contact.user_name
+        return a_cont    
     
+    
+    def __init__(self,account,userName, password):
+        """
+        method that defines user credentials to be stored
+        """
+        self.account = account
+        self.user_name = userName
+        self.password = password
+        
+        
+    def __init__(self,account,userName, password):
+        """
+        method that defines user credentials to be stored
+        """
+        self.account = account
+        self.userName = userName
+        self.password = password
+
+    def save_details(self):
+        """
+        method to store a new credential to the credentials list
+        """
+        Credentials.credentials_list.append(self)
+
+    def delete_credentials(self):
+        """
+        delete_credentials method that deletes an account credentials from the credentials_list
+        """
+        Credentials.credentials_list.remove(self)
     
     
     
